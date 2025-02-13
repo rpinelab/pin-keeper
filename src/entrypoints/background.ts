77f1ef58fd4cc8111e226b3e6461121c4f682f5d
pinelibg <pinelibg@outlook.jp>
2025-02-13 18:00:31 +0900

    fix: disable some ESLint rules for background script

--- src/entrypoints/background.ts
+++ src/entrypoints/background.ts
@@ -1,4 +1,5 @@
 export default defineBackground(() => {
+  // eslint-disable-next-line @typescript-eslint/no-misused-promises, @typescript-eslint/no-unnecessary-condition
   (browser.action ?? browser.browserAction).onClicked.addListener(async () => {
     await restorePinnedTabs();
   });
