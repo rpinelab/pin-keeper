48d238ca0abcc34b87e427000c5a753af4b8a096
pinelibg <pinelibg@outlook.jp>
2025-02-13 16:30:25 +0900

    feat: update project name and description

--- wxt.config.ts
+++ wxt.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'wxt';
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
+  manifest: {
+    name: 'Pin Keeper - Smart Pinned Tab Manager',
+  },
   extensionApi: 'chrome',
   srcDir: 'src',
   modules: ['@wxt-dev/module-react'],
