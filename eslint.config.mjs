9bf90c7fa684a5ea36678564da4b76492aef4621
pinelibg <pinelibg@outlook.jp>
2025-02-15 18:21:23 +0900

    feat: add UI components and theme provider from shadcn

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -23,6 +23,7 @@ export default tseslint.config(
     },
     rules: {
       '@typescript-eslint/no-non-null-assertion': 'off',
+      '@typescript-eslint/no-unnecessary-condition': 'off',
     },
   },
   eslintConfigPrettier,
--- src/entrypoints/options/App.tsx
+++ src/entrypoints/options/App.tsx
@@ -1,3 +1,6 @@
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+
 function App() {
   const [pinnedUrls, setPinnedUrls] = useState<string[]>([
     'https://www.google.com',
@@ -14,23 +17,22 @@ function App() {
   };
 
   return (
-    <main className='p-4'>
-      <h1 className='text-3xl my-2'>PinKeeper Option</h1>
-      <h2 className='text-xl my-2'>Pinned URLs</h2>
-      <ul className='list-disc ml-4 my-2'>
-        {pinnedUrls.map((url, index) => (
-          <li key={index}>{url}</li>
-        ))}
-      </ul>
-      <form className='my-2' action={addUrl}>
-        <input name='url' type='text' className='bg-white text-black' />
-        <button
-          type='submit'
-          className='bg-gray-300 text-black rounded-sm mx-2 px-2 py-1'
-        >
-          Add
-        </button>
-      </form>
+    <main className='flex flex-col gap-4'>
+      <h1 className='text-3xl'>PinKeeper Option</h1>
+      <h2 className='text-xl'>Pinned URLs</h2>
+      <section className='flex flex-col gap-2'>
+        <ul className='m-2 p-2'>
+          {pinnedUrls.map((url, index) => (
+            <li key={index}>
+              <Input name='url' type='text' readOnly value={url} />
+            </li>
+          ))}
+        </ul>
+        <form className='flex gap-2 p-2' action={addUrl}>
+          <Input name='url' type='text' placeholder='URL' />
+          <Button type='submit'>Add</Button>
+        </form>
+      </section>
     </main>
   );
 }
